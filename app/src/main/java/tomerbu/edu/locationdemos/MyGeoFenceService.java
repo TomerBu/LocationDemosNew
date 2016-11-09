ec2fbf597f5a9d4527f0476736d68b67dc4c29b2
tomerbuzaglo <tomer.bu@gmail.com>
2016-11-09 13:34:50 +0200

    Geofence event handling

--- app/src/main/java/tomerbu/edu/locationdemos/MyGeoFenceService.java
+++ app/src/main/java/tomerbu/edu/locationdemos/MyGeoFenceService.java
@@ -2,6 +2,11 @@ package tomerbu.edu.locationdemos;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.location.Location;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
 
 public class MyGeoFenceService extends IntentService {
     /**
@@ -15,6 +20,27 @@ public class MyGeoFenceService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+        if (geofencingEvent.hasError()){
+            Log.d(Constants.TAG, String.format("Error code: %d", geofencingEvent.getErrorCode()));
+            return;
+        }
+
+        Location location = geofencingEvent.getTriggeringLocation();
+        int geofenceTransition = geofencingEvent.getGeofenceTransition();
+
+        switch (geofenceTransition){
+            case Geofence.GEOFENCE_TRANSITION_EXIT:
+                Log.d(Constants.TAG, "Exit" + location.toString());
+                break;
+            case Geofence.GEOFENCE_TRANSITION_ENTER:
+                Log.d(Constants.TAG, "Enter" + location.toString());
+                break;
+            case Geofence.GEOFENCE_TRANSITION_DWELL:
+                Log.d(Constants.TAG, "Dwell" + location.toString());
+                break;
+        }
+
 
     }
 }
